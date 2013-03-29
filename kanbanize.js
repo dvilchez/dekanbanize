52cbe290373d9f69c817976d58f98e6675db2df2
David Vílchez <dvilchezm@gmail.com>
2013-03-29 10:18:00 +0100

    getBoardSettings

--- kanbanize.js
+++ kanbanize.js
@@ -3,19 +3,27 @@ var kanbanize = {};
 
 kanbanize.getProjectsAndBoards = function(callback){
 	rest.post(this.uriProjectAndBoards
-		,{headers:{'apikey':this.apiKey}}).on('complete', function(data){callback(data)})
+		,{headers:{'apikey':this.apiKey}}).on('complete', function(data){callback(JSON.parse(data));})
 };
 
 kanbanize.getBoardStructure = function(idBoard,callback){
 	rest.postJson(this.uriBoardStructure
 		,{'boardid':idBoard}
-		,{headers:{'apikey':this.apiKey}}).on('complete',function(data){callback(data)});
+		,{headers:{'apikey':this.apiKey}}).on('complete',function(data){callback(JSON.parse(data));});
+};
+
+kanbanize.getBoardSettings = function  (idBoard, callback) {
+	rest.postJson(this.uriBoardSettings
+		,{'boardid':idBoard}
+		,{headers:{'apikey':this.apiKey}}).on('complete',function(data){callback(JSON.parse(data));});
 };
 
 module.exports = function(apiKey){
 	kanbanize.domain = 'http://kanbanize.com';
 	kanbanize.uriProjectAndBoards=kanbanize.domain+'/index.php/api/kanbanize/get_projects_and_boards/format/json';
 	kanbanize.uriBoardStructure=kanbanize.domain+'/index.php/api/kanbanize/get_board_structure/format/json';
+	kanbanize.uriBoardSettings=kanbanize.domain+'/index.php/api/kanbanize/get_board_settings/format/json';
+
 	kanbanize.apiKey=apiKey;
 	return kanbanize;
 };
--- test/kanbanize.tests.js
+++ test/kanbanize.tests.js
@@ -5,17 +5,32 @@ var kanbanize=require("../kanbanize.js");
 
 describe("kanbanize",function(){
 	var apiKey="anykey";
+	var idBoard='2';
+	var projectsAndBoards={projects:[{name:'project',id:'idProject',bords:[{name:'board',id:'idBoard'}]}]};
+	var structure={columns:[{position:0,lcname:'juas',description:'tachan'}],lanes:[{lcname:'test',color:'yellow',description:'descripcion'}]};
+	var settings={usernames:['username'],templates:['template'],types:['TYPE']};
+	var scope=nock('http://kanbanize.com')
+		.matchHeader('apikey', apiKey)
+		.post('/index.php/api/kanbanize/get_projects_and_boards/format/json')
+		.reply(200, projectsAndBoards)
+		.post('/index.php/api/kanbanize/get_board_structure/format/json',{boardid:idBoard})
+		.reply(200,structure)
+		.post('/index.php/api/kanbanize/get_board_settings/format/json',{boardid:idBoard})
+		.reply(200,settings);
+
+
+	describe("setXmlResponse", function(){
+		it('should return xml data');
+	});
+
+	describe('setJSONResponse', function(){
+		it('should return json data');
+	});
 
 	describe("getProjectsAndBoards", function(){
 		it('should get projects and boards', function(done){
-			var projectsAndBoards={projects:[{name:'project',id:'idProject',bords:[{name:'board',id:'idBoard'}]}]};
-			var scope=nock('http://kanbanize.com')
-				.matchHeader('apikey', apiKey)
-				.post('/index.php/api/kanbanize/get_projects_and_boards/format/json')
-				.reply(200, projectsAndBoards);
-
 			kanbanize(apiKey).getProjectsAndBoards(function(data){
-				JSON.parse(data).should.eql(projectsAndBoards);
+				data.should.eql(projectsAndBoards);
 				done();
 			});
 		});
@@ -23,16 +38,18 @@ describe("kanbanize",function(){
 
 	describe("getBoardStructure",function(){
 		it('should get the board structure', function(done)
-		{
-			var idBoard='2';
-			var structure={columns:[{position:0,lcname:'juas',description:'tachan'}],lanes:[{lcname:'test',color:'yellow',description:'descripcion'}]};
-			var scope=nock('http://kanbanize.com')
-				.matchHeader('apikey', apiKey)
-				.post('/index.php/api/kanbanize/get_board_structure/format/json',{boardid:idBoard})
-				.reply(200,structure);
-			
+		{	
 			kanbanize(apiKey).getBoardStructure(idBoard,function(data){
-				JSON.parse(data).should.eql(structure);
+				data.should.eql(structure);
+				done();
+			});
+		});
+	});
+
+	describe("getBoardSettings", function(){
+		it('should get the board settings', function (done) {
+			kanbanize(apiKey).getBoardSettings(idBoard, function (data) {
+				data.should.eql(settings);
 				done();
 			});
 		});
