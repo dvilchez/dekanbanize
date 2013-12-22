62cf96ea71ebc260c5c6d38209e93ebd93710e3f
David Vilchez <dvilchez@xuaps.com>
2013-12-22 23:33:59 +0100

    deleted test.js

--- test.js
+++
@@ -1,7 +0,0 @@
-	var rest=require('restler');
-
-	rest.post('http://kanbanize.com/index.php/api/kanbanize/get_board_structure/boardid/2',{headers:{'apikey':'OEypAZ9DSdwq0WZwZBTeZUK7wp5UN0B1JMSBiAry'}})
-		.on('complete', function(data){
-				console.log(data);
-			}
-		);
